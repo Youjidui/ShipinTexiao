2ca810962635b5f37e5c4caa74b511f2f43c83e3
FanBin <fanbin9988@gmail.com>
2015-11-30 01:13:43 +0800

    Init rings render

---
+++ EffRings/RingsRender.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../D3D9Render/RenderEngine.h"
+#include "../D3D9Render/VideoBuffer.h"
+#include "../FxParam.h"
+
+class CRingsRender
+{
+public:
+	CRingsRender(void);
+	~CRingsRender(void);
+
+	bool Init(CRenderEngine* pEngine);
+	void Uninit();
+	bool Render(CVideoBuffer* pDst, CVideoBuffer* pSrc, FxParamBase* pParam);
+
+private:
+	CRenderEngine*	m_pEngine;
+	CBaseMesh*			m_pQuadMesh;
+	CBaseFx*			m_pNegativeEffect;
+};
